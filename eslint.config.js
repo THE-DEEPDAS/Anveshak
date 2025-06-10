f41ac9e9712c7a5917153d009404c15dad6ed995
THE-DEEPDAS <deepdblm@gmail.com>
2025-06-10 14:50:56 +0530

    Root folder cleared

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,35 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["dist"],
+    extends: [js.configs.recommended], // Use recommended rules from ESLint's core
+    files: ["**/*.{js,jsx}"], // Types of flies batave which are to be linted
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser, // like window, document, etc.
+        React: "readonly", // Add React global
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      // Common JavaScript rules
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
     },
-  }
-);
+  },
+];
